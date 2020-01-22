351fce52af023f8d8c67e733b6b7b30e833e5ddb
chatan000 <58610792+chatan000@users.noreply.github.com>
2020-01-22 13:31:16 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-function life () {
-    info.setLife(1)
-}
 function Score () {
     info.changeScoreBy(1)
 }
@@ -12,54 +9,57 @@ function overlap () {
 }
 function Surivior () {
     surivior = sprites.create(img`
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . f f f . . . . . . . 
-. . . . . . . . . . . . . f 4 f f f f f . . . . 
-. . . . . . . . . . . f f 4 4 e e e e e f f . . 
-. . . . . . . . . . f f 4 4 4 e e e e e e f f . 
-. . . . . . . . . . f e e e e f f f e e e e f . 
-. . . . . . . . . f e 4 4 4 4 e e e f f f f f . 
-. . . . . . . . . f 4 e f f f f f 2 2 2 e f f f 
-. . . . . . . . . f f f e e e f f f f f f f f f 
-. . . . . . . . . f 4 4 4 4 f b b 4 9 9 4 f 4 f 
-. . . . . . . . . . f 4 d d f b b d d 9 4 4 f . 
-. . . . . . . . . 4 4 f d d d d d d 4 4 4 f . . 
-. . . . . . . . . d d 4 4 2 2 2 2 2 2 2 f . . . 
-. . . . . . . . . d d 4 4 4 8 9 9 9 9 9 f . . . 
-. . . . . . . . . 4 4 4 4 f f f f f f f f . . . 
-. . . . . . . . . . . . . f f . . . f f f . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . 1 1 . . . . . . . . . . . . . . 1 1 . . . 
+. . 1 1 . . . . . . . . . . . . . . . . 1 1 . . 
+. 1 1 . . . . . . . . . . . . . . . . . . 1 1 . 
+. 1 . . . . . . . . . . . . . . . . . . . . 1 . 
+1 1 . . . . . . . . . . . . . . . . . . . . 1 1 
 `, SpriteKind.Player)
     surivior.setPosition(128, 63)
-    controller.moveSprite(surivior, 100, 100)
+    controller.moveSprite(surivior, 40, 100)
     surivior.setFlag(SpriteFlag.StayInScreen, true)
 }
 function projectile2 () {
     projectile = sprites.createProjectileFromSide(img`
-. . . . . . . . . c c c . . . . 
-. . . . . . c c c c c c c c . . 
-. . . b b b b b b b b b b c c . 
-. . b c c c b b c c b b b b b c 
-c b c c c c c c c c b b b c b c 
-c c c c c c b b b c c b c c b c 
-c c c c c b c c b b c b b c b c 
-b c b b c b b b c b b c b c b c 
-b c b b c c c c c c b c b c c c 
-b c b b b c b b c c b c b c c c 
-. c c c b c b b b c c c b c c c 
-. . c c c c c b c b c c c c c c 
-. . . b b b c c c c c c c c c . 
-. . . c c b b b b c c c c c c . 
-. . . . c c b b b c c c . . . . 
-. . . . . c c c c c c . . . . . 
-`, 50, Math.randomRange(-5, 98))
+. . . . . . . . . . . . . . . . 
+. . . . . c c c c c . . . . . . 
+. . . . c c b b c c c c . . . . 
+. . . c c b b b b b c c . . . . 
+. . c c c b b b b c c c c c c . 
+c c c c c c c c c c c c c c c c 
+c c c c c b c c c c b b c c c c 
+c c c b b b c c c c b c c c c c 
+c c c b b c c c b c b c c c c c 
+c c c b c c c c b b b c c c c . 
+. c c b b b c c c c c c c c c . 
+. c c c c c c c c c c c c c c . 
+. c c c c c c b b b b b c c c . 
+. . c c c b b b b b b c c c . . 
+. . . c c c b b b b b c c . . . 
+. . . . . c c c c c c c . . . . 
+`, 50, Math.randomRange(-5, 97))
+}
+function life () {
+    info.setLife(1)
 }
 let projectile: Sprite = null
 let surivior: Sprite = null
