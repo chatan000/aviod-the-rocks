f7ae907393aa199f968a8f2d99c293c55781bf59
chatan000 <58610792+chatan000@users.noreply.github.com>
2020-01-24 13:58:25 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,33 +9,33 @@ function overlap () {
 }
 function Surivior () {
     surivior = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . e e e e e e e e e e e e e e . . . . . 
 . . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
 . . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
 . . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
 . . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
 . . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
-. . . . . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . . . . 
+. . . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . . . . . 
+. . . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . . . . . 
+. . . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . . . . . 
+. . . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 9 . . . . . 
 . . . 1 1 . . . . . . . . . . . . . . 1 1 . . . 
 . . 1 1 . . . . . . . . . . . . . . . . 1 1 . . 
 . 1 1 . . . . . . . . . . . . . . . . . . 1 1 . 
-. 1 . . . . . . . . . . . . . . . . . . . . 1 . 
 1 1 . . . . . . . . . . . . . . . . . . . . 1 1 
+. . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
     surivior.setPosition(128, 63)
-    controller.moveSprite(surivior, 40, 100)
+    controller.moveSprite(surivior, 100, 100)
     surivior.setFlag(SpriteFlag.StayInScreen, true)
 }
 function projectile2 () {
@@ -56,14 +56,14 @@ c c c b c c c c b b b c c c c .
 . . c c c b b b b b b c c c . . 
 . . . c c c b b b b b c c . . . 
 . . . . . c c c c c c c . . . . 
-`, 50, Math.randomRange(-5, 97))
+`, 50, Math.randomRange(-10, 90))
 }
 function life () {
     info.setLife(1)
 }
 let projectile: Sprite = null
 let surivior: Sprite = null
-game.splash("Survive the falling rocks for as long as you can")
+game.splash("Survive the falling rocks for as long as you can.If you get 100 points or more you get your name in the game.")
 Surivior()
 life()
 music.baDing.play()
